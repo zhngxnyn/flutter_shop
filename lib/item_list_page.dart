5ab42167781acfd99edc14fec4ed179394859f31
현원 <jhwjhwjhw97@gmail.com>
2024-10-31 23:27:23 +0900

    UI 구성2

--- lib/item_list_page.dart
+++ lib/item_list_page.dart
@@ -1,7 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/constants.dart';
+import 'package:flutter_shop/item_details_page.dart';
 import 'package:flutter_shop/models/product.dart';
-import 'package:intl/intl.dart';
 
 class ItemListPage extends StatefulWidget {
   const ItemListPage({super.key});
@@ -11,8 +12,6 @@ class ItemListPage extends StatefulWidget {
 }
 
 class _ItemListPageState extends State<ItemListPage> {
-  final NumberFormat numberFormat = NumberFormat('###,###,###,###');
-
   List<Product> productList = [
     Product(
         productNo: 1,
@@ -61,6 +60,7 @@ class _ItemListPageState extends State<ItemListPage> {
           ),
           itemBuilder: (context, index) {
             return productContainer(
+              productNo: productList[index].productNo ?? 0,
               productName: productList[index].productName ??
                   "", // !를 쓰면 실행도중에 에러발생. null이 될 수도 있어서
               productImageUrl: productList[index].productImageUrl ?? "",
@@ -71,46 +71,61 @@ class _ItemListPageState extends State<ItemListPage> {
   }
 
   Widget productContainer({
+    required int productNo,
     required String productName,
     required String productImageUrl,
     required double price,
   }) {
-    return Container(
-      padding: const EdgeInsets.all(5),
-      child: Column(
-        children: [
-          CachedNetworkImage(
-            height: 150,
-            fit: BoxFit.cover,
-            imageUrl: productImageUrl,
-            placeholder: (context, url) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  strokeWidth: 2,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) {
+            return ItemDetailsPage(
+              productNo: productNo,
+              productName: productName,
+              productImageUrl: productImageUrl,
+              price: price,
+            );
+          },
+        ));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              height: 150,
+              fit: BoxFit.cover,
+              imageUrl: productImageUrl,
+              placeholder: (context, url) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2,
+                  ),
+                );
+              },
+              errorWidget: (context, url, error) {
+                // 네트워크 오류 시
+                return const Center(
+                  child: Text("오류 발생"),
+                );
+              },
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                productName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
-              );
-            },
-            errorWidget: (context, url, error) {
-              // 네트워크 오류 시
-              return const Center(
-                child: Text("오류 발생"),
-              );
-            },
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              productName,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Text("${numberFormat.format(price)}원"),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text("${numberFormat.format(price)}원"),
+            ),
+          ],
+        ),
       ),
     );
   }
