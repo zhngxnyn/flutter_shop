8026c8e88a3d39d5b30efcdbe3d3a16a8e4f75ca
현원 <jhwjhwjhw97@gmail.com>
2024-11-02 00:28:52 +0900

    UI 구성_제품 결제시작 페이지(1)

--- lib/item_basket_page.dart
+++ lib/item_basket_page.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/constants.dart';
+import 'package:flutter_shop/item_checkout_page.dart';
 import 'package:flutter_shop/models/product.dart';
 
 class ItemBasketPage extends StatefulWidget {
@@ -66,7 +67,16 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(20),
         child: FilledButton(
-          onPressed: () {},
+          onPressed: () {
+            // 결제시작 페이지로 이동
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return const ItemCheckoutPage();
+                },
+              ),
+            );
+          },
           child: Text("총 ${numberFormat.format(totalPrice)}원 결제하기"),
         ),
       ),
